fa96ec817c5dc4dfcdf3885e98eaf6f9019a51c6
angelo lorensi <angelomozza@gmail.com>
2022-10-29 15:24:53 -0300

    criando o projeto

---
+++ clima-tempo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClimaComponent } from './clima/clima.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClimaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
